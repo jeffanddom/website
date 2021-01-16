7179cc279289edd60a9f0afb1226b5db84b568aa
Jeff Lee <jeffomatic@gmail.com>
2021-01-16 12:34:42 -0800

    replace gatsby with hugo (#3)

--- src/cms/cms.js
+++
@@ -1,9 +0,0 @@
-import CMS from 'netlify-cms-app'
-
-import BroadcastPreview from './preview-templates/BroadcastPreview'
-import DevlogPreview from './preview-templates/DevlogPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
-
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('devlog', DevlogPreview)
-CMS.registerPreviewTemplate('broadcast', BroadcastPreview)
